f765cc411790bee468ef626814cafaaa09bef046
Doma <halimfathin7@gmail.com>
2023-10-22 12:12:42 +0700

    ada yang kelupaan hehe

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ server.get("/search", function (req, res) {
   });
 });
 
-server.post("/edit/:id", function (req, res) {
+server.post("/edit/:id", async function (req, res) {
   const acceptedData = data.find((obj) => obj.id === req.params.id);
   console.log(acceptedData);
   const user = req.body;
@@ -148,7 +148,7 @@ server.post("/edit/:id", function (req, res) {
         ".jpg",
       Diedit: user.pembuat,
       Content: JSON.parse(user.content),
-    }); 
+    });
   }
 
   res.send(dataOnGoing);
@@ -227,15 +227,14 @@ server.get("/accept/:id", async function (req, res) {
       data.push(acceptedData);
 
       await mainModel.create({
-      id: acceptedData.id,
-      Title: acceptedData.Title,
-      Pembuat: acceptedData.Pembuat,
-      Image: acceptedData.Image,
-      Diedit: "",
-      Content: acceptedData.Content,
-      
-    });
-    await goingModel.deleteOne({ id: req.params.id });
+        id: acceptedData.id,
+        Title: acceptedData.Title,
+        Pembuat: acceptedData.Pembuat,
+        Image: acceptedData.Image,
+        Diedit: "",
+        Content: acceptedData.Content,
+      });
+      await goingModel.deleteOne({ id: req.params.id });
     }
 
     // Hapus dataOnGoing berdasarkan ID
